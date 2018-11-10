39858b7aa7cd470c519e56c91f92cd52270ae962
o84118317 <o84118317@gmail.com>
2018-11-10 19:02:29 +0800

    passing data through props

--- my_first_react/src/components/App.js
+++ my_first_react/src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-
+import Game from './Game.js'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <div className='container'>
+				<div>
+					<h1>Game start</h1>
+				</div>
+				<Game />
       </div>
     );
   }
